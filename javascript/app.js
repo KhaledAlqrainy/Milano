52202cba17c9ea4600e6929416e0b93309eb44a4
KhaledAlqrainy <>
2021-05-05 15:21:22 +0000

    add js

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+alert ('Welcom To Milano Store')
+var gender = prompt ('whats your gender')
+if (gender === 'Male'){
+document.write('<img src="https://i.pinimg.com/originals/3c/fb/9e/3cfb9e690d95a8c59a8a0375926fff76.jpg">')
+}
+else if (gender === 'Female') {
+document.write('<img src="http://www.cstylejeans.com/wp-content/uploads/2016/01/womens-fashion-clothes-cheap.jpg">')
+}
+else {
+  alert('you should pick between Male or Female')
+}
+
+
