7d312f90d0b7d6d001df83f5a837ecf5cb11d482
KhaledAlqrainy <>
2021-05-07 23:12:44 +0000

    edit readme

--- javascript/app.js
+++ javascript/app.js
@@ -26,5 +26,3 @@ for (var x=0; x < numbers ; x++){
   Image= Image + '<img src="https://i.pinimg.com/564x/94/ed/fe/94edfeeee0a0f8e77b492a8a9375e4f8.jpg">'
 }
 document.write(Image)
-=======
->>>>>>> 52202cba17c9ea4600e6929416e0b93309eb44a4
